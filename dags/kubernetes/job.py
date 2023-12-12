0c3478c99b8c60c525d69e1d18aeaaea970bf8d3
asongent <59109755+asongent@users.noreply.github.com>
2023-12-12 15:32:17 -0500

    Update job.py

--- dags/kubernetes/job.py
+++ dags/kubernetes/job.py
@@ -1,1 +1,19 @@
 
+from airflow import DAG
+from datetime import datetime, timedelta
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2023, 1, 1),
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+}
+
+dag = DAG(
+    'spark_example',
+    default_args=default_args,
+    description='A simple Spark DAG',
+    schedule_interval=timedelta(days=1),
+)
+
