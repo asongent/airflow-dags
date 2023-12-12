20634b09ae5ddef6196d5a8eb74634d1923077eb
asongent <59109755+asongent@users.noreply.github.com>
2023-12-12 15:34:50 -0500

    Update sample2.py

--- dags/sample2.py
+++ dags/sample2.py
@@ -1,1 +1,11 @@
+from airflow.contrib.operators.spark_submit_operator import SparkSubmitOperator
 
+spark_task = SparkSubmitOperator(
+    task_id='spark_task',
+    conn_id='spark',
+    application='/kubernetes/job.py',
+    verbose=False,
+    conf={'key': 'value'},
+    application_args=['arg1', 'arg2'],
+    dag=dag,
+)
